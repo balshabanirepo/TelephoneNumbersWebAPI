d4e9d263adc3db310be1140ac2c5820c4df968cb
Basem AlShabani <balshabani@link.sa>
2021-02-28 16:09:42 +0200

    Add project files.

---
+++ TelephoneNumbersWebAPI/Controllers/TelephoneDirectoriesController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using TelephoneNumbersWebAPI.Models;
+
+namespace VaccinationAppointmentVerificationWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TelephoneDirectoriesController : ControllerBase
+    {
+        //private readonly dbContext _context;
+
+        List<TelephoneDirectory> Telephones = new List<TelephoneDirectory>
+        {
+            new TelephoneDirectory
+            {
+                Id=1,PhoneNumber="543210"
+            },
+              new TelephoneDirectory
+            {
+                Id=1,PhoneNumber="98765"
+            }
+        };
+        public TelephoneDirectoriesController()
+        {
+           // _context = context;
+        }
+
+        // GET: api/TelephoneDirectories
+        [HttpGet]
+        public ActionResult<IEnumerable<TelephoneDirectory>> GetTelephones()
+        {
+            return Telephones.ToList();
+        }
+
+        // GET: api/TelephoneDirectories/5
+        [HttpGet("{id}")]
+        public ActionResult<TelephoneDirectory> GetTelephoneDirectory(int id)
+        {
+            var telephoneDirectory = Telephones.Find(f=>f.Id==id);
+
+            if (telephoneDirectory == null)
+            {
+                return NotFound();
+            }
+
+            return telephoneDirectory;
+        }
+        [HttpGet]
+        [Route("PhoneByNumber")]
+        public ActionResult<TelephoneDirectory> PhoneByNumber(string Number)
+        {
+            var telephoneDirectory = Telephones.Where(w => w.PhoneNumber == Number).FirstOrDefault();
+            if (telephoneDirectory == null)
+            {
+                return NotFound();
+            }
+
+            return telephoneDirectory;
+        }
+        // PUT: api/TelephoneDirectories/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        //[HttpPut("{id}")]
+        //public async Task<IActionResult> PutTelephoneDirectory(int id, TelephoneDirectory telephoneDirectory)
+        //{
+        //    if (id != telephoneDirectory.Id)
+        //    {
+        //        return BadRequest();
+        //    }
+
+        //    _context.Entry(telephoneDirectory).State = EntityState.Modified;
+
+        //    try
+        //    {
+        //        await _context.SaveChangesAsync();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        if (!TelephoneDirectoryExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
+
+        //    return NoContent();
+        //}
+
+        //// POST: api/TelephoneDirectories
+        //// To protect from overposting attacks, enable the specific properties you want to bind to, for
+        //// more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        //[HttpPost]
+        //public async Task<ActionResult<TelephoneDirectory>> PostTelephoneDirectory(TelephoneDirectory telephoneDirectory)
+        //{
+        //    _context.Telephones.Add(telephoneDirectory);
+        //    await _context.SaveChangesAsync();
+
+        //    return CreatedAtAction("GetTelephoneDirectory", new { id = telephoneDirectory.Id }, telephoneDirectory);
+        //}
+
+        //// DELETE: api/TelephoneDirectories/5
+        //[HttpDelete("{id}")]
+        //public async Task<ActionResult<TelephoneDirectory>> DeleteTelephoneDirectory(int id)
+        //{
+        //    var telephoneDirectory = await _context.Telephones.FindAsync(id);
+        //    if (telephoneDirectory == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    _context.Telephones.Remove(telephoneDirectory);
+        //    await _context.SaveChangesAsync();
+
+        //    return telephoneDirectory;
+        //}
+
+        //private bool TelephoneDirectoryExists(int id)
+        //{
+        //    return _context.Telephones.Any(e => e.Id == id);
+        //}
+    }
+}
---
+++ TelephoneNumbersWebAPI/Controllers/TelephoneDirectory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TelephoneNumbersWebAPI.Models
+{
+    public class TelephoneDirectory
+    {
+        [Key]
+        public int Id { get; set; }
+       
+        public string PhoneNumber { get; set; }
+
+      
+    }
+}
